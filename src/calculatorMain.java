680b7dd37c8ba83cab92480ea2a7dabb710051ef
minho <minho@DESKTOP-4VUN4AS>
2018-03-06 17:52:12 +0900

    사칙연산에 따른 계산

--- src/calculatorMain.java
+++ src/calculatorMain.java
@@ -12,10 +12,22 @@ public class calculatorMain {
 		int second = sc.nextInt();
 		System.out.println("두번째 입력값: "+second);
 		
-		System.out.println("더하기"+ (first+second));
-		System.out.println("빼기"+ (first-second));
-		System.out.println("곱하기"+ (first*second));
-		System.out.println("나누기"+ (first/second));
+		System.out.println("연산 부호 입력");
+		String symbol = sc.next();
+		
+		if("+".equals(symbol)) {
+			System.out.println("더하기 값: "+ (first+second));
+		}else if ("-".equals(symbol)) {
+			System.out.println("빼기 값: "+ (first-second));
+		}else if ("-".equals(symbol)) {
+			System.out.println("곱하기 값: "+ (first*second));
+		}else if("/".equals(symbol)){
+			System.out.println("나누기 값: "+ (first/second));
+		}else {
+			System.out.println("사칙연산에 해당하지 않는 값을 입력");
+		}
+		
+		
 }
 
 }
