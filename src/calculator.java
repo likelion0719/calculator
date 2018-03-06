c54a8706ebd2a5f1b6cb45846d4c9e85bbb248f8
minho <minho@DESKTOP-4VUN4AS>
2018-03-06 18:51:55 +0900

    기능별 메소드로 분리

--- src/calculator.java
+++ src/calculator.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
 
 public class calculator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int getFirstValue(Scanner sc) {
+		System.out.println("첫번째 수 입력");
+		int first = sc.nextInt();
+		return first;
+	}
+	public static int getSecondValue(Scanner sc) {
+		System.out.println("두번째 수 입력");
+		int second = sc.nextInt();
+		return second;
+	}
+	public static String getSymbol(Scanner sc) {
+		System.out.println("사칙연산 입력");
+		String symbol = sc.next();
+		return symbol;
+	}
+	public static int calculate(String symbol,int first, int second) {
+		if("+".equals(symbol)) {
+			first+=second;
+			System.out.println("더하기값 : "+first);
+		}else if ("-".equals(symbol)) {
+			first-=second;
+			System.out.println("빼기값 : "+first);
+		}else if ("*".equals(symbol)) {
+			first*=second;
+			System.out.println("곱하기값 : "+first);
+		}else if("/".equals(symbol)){
+			first/=second;
+			System.out.println("나누기값 : "+first);
+		}else {
+			System.out.println("사칙연산에 해당하지 않는 값을 입력");
+		}
+		return first;
+		
+	}
+	
+	public static void printResult(int result) {
+		System.out.println("결과 값: "+result);
 	}
 
 }
--- src/calculatorMain.java
+++ src/calculatorMain.java
@@ -4,36 +4,17 @@ public class calculatorMain {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("첫번째 입력값");
-		int first = sc.nextInt();
-		System.out.println("첫번째 입력값: "+first);
-		String symbol;
-		int second = 0;
-		
+		int first = calculator.getFirstValue(sc);
+		int result = first;
 		while(true) {
-			System.out.println("연산 부호 입력");
-			 symbol = sc.next();
+			String symbol = calculator.getSymbol(sc);
 			
 			if("quit".equals(symbol)) {
-				System.out.println("결과: "+first);
+				calculator.printResult(result);
 				break;
 			}else {
-				System.out.println("두번째 입력값");
-				second = sc.nextInt();
-				System.out.println("두번째 입력값: "+second);
-
-				if("+".equals(symbol)) {
-					first+=second;
-				}else if ("-".equals(symbol)) {
-					first-=second;
-				}else if ("*".equals(symbol)) {
-					first*=second;
-				}else if("/".equals(symbol)){
-					first/=second;
-				}else {
-					System.out.println("사칙연산에 해당하지 않는 값을 입력");
-				}
-
+				int second = calculator.getSecondValue(sc);
+				result =calculator.calculate(symbol, result, second);
 			}
 				
 						
